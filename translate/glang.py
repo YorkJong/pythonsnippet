d95bc32e41697c07b2d49ab8337207696bd89170
yukuan jiang <yukuan.jiang@c2fcd59a-08bf-11de-b5bf-434079a3519f>
2009-03-04 14:33:55 +0000

    Adopted Python decorators.

--- translate/glang.py
+++ translate/glang.py
@@ -1,13 +1,141 @@
+# -*- encoding: utf-8 -*-
 """
 A wrapper of the AJAX Language API
 
 ref. http://code.google.com/p/python-googlelanguage/source/browse/trunk/googlelanguage/__init__.py
 ref. http://code.google.com/apis/ajaxlanguage/documentation/reference.html#_intro_fonje
 """
+__author__ = "Jiang Yu-Kuan, yukuan.jiang(at)gmail.com"
+__date__ = "2009/03/03 (initial)"
+__version__ = "1.0"
 
-import urllib, simplejson
+import urllib
+
+import simplejson
 
 
+# from http://code.google.com/apis/ajaxlanguage/documentation/reference.html#LangNameArray
+_langCode = {
+    'AFRIKAANS' : 'af',
+    'ALBANIAN' : 'sq',
+    'AMHARIC' : 'am',
+    'ARABIC' : 'ar',
+    'ARMENIAN' : 'hy',
+    'AZERBAIJANI' : 'az',
+    'BASQUE' : 'eu',
+    'BELARUSIAN' : 'be',
+    'BENGALI' : 'bn',
+    'BIHARI' : 'bh',
+    'BULGARIAN' : 'bg',
+    'BURMESE' : 'my',
+    'CATALAN' : 'ca',
+    'CHEROKEE' : 'chr',
+    'CHINESE' : 'zh',
+    'CHINESE_SIMPLIFIED' : 'zh-CN',
+    'CHINESE_TRADITIONAL' : 'zh-TW',
+    'CROATIAN' : 'hr',
+    'CZECH' : 'cs',
+    'DANISH' : 'da',
+    'DHIVEHI' : 'dv',
+    'DUTCH': 'nl',
+    'ENGLISH' : 'en',
+    'ESPERANTO' : 'eo',
+    'ESTONIAN' : 'et',
+    'FILIPINO' : 'tl',
+    'FINNISH' : 'fi',
+    'FRENCH' : 'fr',
+    'GALICIAN' : 'gl',
+    'GEORGIAN' : 'ka',
+    'GERMAN' : 'de',
+    'GREEK' : 'el',
+    'GUARANI' : 'gn',
+    'GUJARATI' : 'gu',
+    'HEBREW' : 'iw',
+    'HINDI' : 'hi',
+    'HUNGARIAN' : 'hu',
+    'ICELANDIC' : 'is',
+    'INDONESIAN' : 'id',
+    'INUKTITUT' : 'iu',
+    'ITALIAN' : 'it',
+    'JAPANESE' : 'ja',
+    'KANNADA' : 'kn',
+    'KAZAKH' : 'kk',
+    'KHMER' : 'km',
+    'KOREAN' : 'ko',
+    'KURDISH': 'ku',
+    'KYRGYZ': 'ky',
+    'LAOTHIAN': 'lo',
+    'LATVIAN' : 'lv',
+    'LITHUANIAN' : 'lt',
+    'MACEDONIAN' : 'mk',
+    'MALAY' : 'ms',
+    'MALAYALAM' : 'ml',
+    'MALTESE' : 'mt',
+    'MARATHI' : 'mr',
+    'MONGOLIAN' : 'mn',
+    'NEPALI' : 'ne',
+    'NORWEGIAN' : 'no',
+    'ORIYA' : 'or',
+    'PASHTO' : 'ps',
+    'PERSIAN' : 'fa',
+    'POLISH' : 'pl',
+    'PORTUGUESE' : 'pt-PT',
+    'PUNJABI' : 'pa',
+    'ROMANIAN' : 'ro',
+    'RUSSIAN' : 'ru',
+    'SANSKRIT' : 'sa',
+    'SERBIAN' : 'sr',
+    'SINDHI' : 'sd',
+    'SINHALESE' : 'si',
+    'SLOVAK' : 'sk',
+    'SLOVENIAN' : 'sl',
+    'SPANISH' : 'es',
+    'SWAHILI' : 'sw',
+    'SWEDISH' : 'sv',
+    'TAJIK' : 'tg',
+    'TAIWAN' : 'zh-TW',
+    'TAMIL' : 'ta',
+    'TAGALOG' : 'tl',
+    'TELUGU' : 'te',
+    'THAI' : 'th',
+    'TIBETAN' : 'bo',
+    'TURKISH' : 'tr',
+    'UKRAINIAN' : 'uk',
+    'URDU' : 'ur',
+    'UZBEK' : 'uz',
+    'UIGHUR' : 'ug',
+    'VIETNAMESE' : 'vi',
+    'UNKNOWN' : ''
+}
+
+
+def language_name(func):
+    def wrapper(text, src, dest):
+        lang_names = _langCode.keys()
+        src, dest = src.upper(), dest.upper()
+
+        if src in lang_names:
+            src = _langCode.get(src, "auto")
+        if dest in lang_names:
+            dest = _langCode.get(dest, "auto")
+
+        return func(text, src, dest)
+
+    return wrapper
+
+
+def unicode_text(func):
+    def wrapper(text, *args, **kwargs):
+        if isinstance(text, unicode):
+            text = text.encode("utf8")
+
+        return func(text, *args, **kwargs)
+
+    return wrapper
+
+
+@unicode_text
+@language_name
 def translate(text, srcLang='en', destLang='zh-TW'):
     """Returns translated text for the given text supplied, matching the
     destination language.
@@ -15,13 +143,24 @@ def translate(text, srcLang='en', destLang='zh-TW'):
     Arguments
     ---------
     text     - The text that is to be translated.
-    srcLang  - The source language as a language code
-    destLang - The destination language as a language code.
-    ---------
+    srcLang  - The source language
+    destLang - The destination language
 
+    Example
+    -------
+    >>> translate("世界你好", "zh-TW", "en")
+    u'Hello World'
+    >>> translate("世界你好".decode("utf8"), "zh-TW", "fr")
+    u'Bonjour tout le monde'
+    >>> translate("Bonjour tout le monde", "fr", "en")
+    u'Hello World'
+    >>> translate("ハローワールド", "Japanese", "en")
+    u'Hello World'
+    >>> translate("A bird can fly high.", "en", "fr")
+    u'Un oiseau peut voler haut.'
     """
     TRANSLATE_URL = 'http://ajax.googleapis.com/ajax/services/language/translate?'
-    TRANSLATE_PARAMS = {'v':'1.0', 'q':'', 'langpair': ''}
+    TRANSLATE_PARAMS = {'v':'1.0', 'q':'', 'langpair':''}
 
     params = TRANSLATE_PARAMS
     params['q'] = text
@@ -42,6 +181,7 @@ def translate(text, srcLang='en', destLang='zh-TW'):
     return None
 
 
+@unicode_text
 def detect(text):
     """Return the language code that describes the language of the given text.
 
@@ -49,14 +189,17 @@ def detect(text):
     ---------
     text - The text that is to be translated.
 
-
     Example
     -------
-    >>> detect("What the language.")
+    >>> detect("Stand Alone Complex")
     u'en'
+    >>> detect("將麵和面一起服下肚")
+    u'zh-TW'
+    >>> detect("面皮")
+    u'zh-CN'
     """
     DETECT_URL = 'http://ajax.googleapis.com/ajax/services/language/detect?'
-    DETECT_PARAMS = {'v':'1.0', 'q':'', }
+    DETECT_PARAMS = {'v':'1.0', 'q':''}
 
     params = DETECT_PARAMS
     params['q'] = text
@@ -77,6 +220,7 @@ def detect(text):
 
 
 if __name__ == "__main__":
-    print translate("Pass the test.")
-    #import doctest
-    #doctest.testmod()
+    import doctest
+    doctest.testmod()
+    print translate("世界你好".decode("utf8"), "zh-TW", "fr")
+    print translate("ハローワールド", "Japanese", "en")
