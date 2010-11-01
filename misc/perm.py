aed73991e2fb56a743d93325ec30db2cfa2e095e
yukuan jiang <yukuan.jiang@gmail.com@c2fcd59a-08bf-11de-b5bf-434079a3519f>
2010-11-01 17:50:32 +0000

    perm.py:   - added one_line_from_cycles function   - a lot of refactoring

--- misc/perm.py
+++ misc/perm.py
@@ -11,8 +11,8 @@ http://en.wikipedia.org/wiki/Permutation
 http://en.wikipedia.org/wiki/Permutation_group
 """
 __author__ = "Jiang Yu-Kuan, yukuan.jiang(at)gmail.com"
-__date__ = "2006/10/25"
-__revision__ = "1.2"
+__date__ = "2010/10/25~2010/11/02"
+__revision__ = "2.0"
 
 from functools import partial
 
@@ -27,7 +27,7 @@ def gcd(a, b):
         a, b = b, a % b
     return a
 
-    
+
 def lcm(*args):
     """Return least common multiple of args."""
     return reduce(lambda a, b: a * b / gcd(a, b), args)
@@ -77,15 +77,42 @@ def cycles_from_one_line(line):
         cycles.append(cycle)
     return cycles
 
-    
+
+def one_line_from_cycles(cycles):
+    """Return a one-line notation of a permutation from the corresponding cycle
+    notation.
+
+    Note
+    ----
+    In group theory, there are three main notations for permutations of a finite
+    set S: 1) two-line notation; 2) one-line notation; and 3) cycle notation.
+
+    Reference
+    ---------
+    http://en.wikipedia.org/wiki/Permutation
+
+    Example
+    -------
+    >>> cycles = [[0], [1, 5, 7, 8, 4, 2], [3, 6], [9]] # even-odd bipartition
+    >>> one_line_from_cycles(cycles)
+    [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]
+    """
+    pred = {}
+    for c in cycles:
+        n = len(c)
+        pred.update((x, c[(i + n - 1) % n]) for i, x in enumerate(c))
+
+    return [pred[x] for x in range(len(pred))]
+
+
 def order_from_cycles(cycles):
     """Return the order of a cyclic permutation group.
-    
+
     Arguments
     ---------
     cycles
         the cycle notation of a permutation
-        
+
     Example
     -------
     >>> one_line = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]   # even-odd bipartition
@@ -94,10 +121,10 @@ def order_from_cycles(cycles):
     6
     """
     return lcm(*[len(c) for c in cycles])
-    
+
 #------------------------------------------------------------------------------
 
-def permute(seq, line):
+def permute_with_one_line(seq, line):
     """Permute a sequence with a permutation in one-line notation.
 
     Arguments
@@ -111,13 +138,13 @@ def permute(seq, line):
     -------
     >>> seq = range(10)
     >>> one_line = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]   # even-odd bipartition
-    >>> permute(seq, one_line)
+    >>> permute_with_one_line(seq, one_line)
     [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]
     """
     return [line[seq[x]] for x in range(len(seq))]
 
 
-def permute_with_follow_cycles(seq, cycles):
+def permute_with_cycles(seq, cycles):
     """Permute a sequence in place with a permutation in cycle notation.
 
     A follow-the-cycles algorithm is applied to this function.
@@ -138,21 +165,19 @@ def permute_with_follow_cycles(seq, cycles):
     >>> seq = range(10)
     >>> one_line = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]   # even-odd bipartition
     >>> cycles = cycles_from_one_line(one_line)
-    >>> cycles
-    [[0], [1, 5, 7, 8, 4, 2], [3, 6], [9]]
-    >>> permute_with_follow_cycles(seq, cycles)
+    >>> permute_with_cycles(seq, cycles)
     >>> seq
     [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]
     """
-    def succ(cycle, x):
-        n = len(cycle)
-        dic = dict(zip(cycle, range(n)))
-        return cycle[(dic[x] + 1) % n]
-
     def pred(cycle, x):
         n = len(cycle)
         dic = dict(zip(cycle, range(n)))
         return cycle[(dic[x] + n - 1) % n]
+
+    def succ(cycle, x):
+        n = len(cycle)
+        dic = dict(zip(cycle, range(n)))
+        return cycle[(dic[x] + 1) % n]
 
     for cycle in [c for c in cycles if len(c)>1]:
         s = cycle[0]
@@ -164,9 +189,9 @@ def permute_with_follow_cycles(seq, cycles):
         seq[succ(cycle, s)] = d
 
 
-def permute_with_modified_follow_cycles(seq, succ, pred):
-    """Permute a sequence in place with successor and predecessor operations of
-    a permutation.
+def permute_with_pred_and_succ(seq, pred, succ):
+    """Permute a sequence in place with cycle operations of successor and
+    predecessor for a permutation.
 
     A modified follow-the-cycles algorithm is applied to this function.
 
@@ -174,18 +199,18 @@ def permute_with_modified_follow_cycles(seq, succ, pred):
     ---------
     seq
         the sequence to be permuted
-    succ
-        the successor function of a permutation
     pred
         the predecessor function of a permutation
+    succ
+        the successor function of a permutation
 
     Example
     -------
     >>> seq = range(10)
     >>> n = len(seq)
-    >>> succ = partial(succ_of_even_odd, n=n)
     >>> pred = partial(pred_of_even_odd, n=n)
-    >>> permute_with_modified_follow_cycles(seq, succ, pred)
+    >>> succ = partial(succ_of_even_odd, n=n)
+    >>> permute_with_pred_and_succ(seq, pred, succ)
     >>> print seq
     [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]
     """
@@ -205,21 +230,6 @@ def permute_with_modified_follow_cycles(seq, succ, pred):
 # Apply Permutations to Even-odd bipartitions
 #------------------------------------------------------------------------------
 
-def succ_of_even_odd(x, n):
-    """Return successor of x in the cycle of a even-odd permutation.
-
-    Example
-    -------
-    >>> seq = range(10)
-    >>> succ = partial(succ_of_even_odd, n=len(seq))
-    >>> print [succ(x) for x in seq]
-    [0, 5, 1, 6, 2, 7, 3, 8, 4, 9]
-    """
-    if x%2 == 0:
-        return x/2
-    return n/2 + x/2
-
-
 def pred_of_even_odd(x, n):
     """Return predecessor of x in the cycle of a even-odd permutation.
 
@@ -234,27 +244,24 @@ def pred_of_even_odd(x, n):
         return 2 * x
     return 2 * (x - n/2) + 1
 
-#------------------------------------------------------------------------------
-# Apply Permutations to Picture Rotations
-#------------------------------------------------------------------------------
 
-def succ_of_rotate90cw(i, w, h):
-    """Return successor of i in the cycle of a permutation of rotation 90
-    degree clockwise.
+def succ_of_even_odd(x, n):
+    """Return successor of x in the cycle of a even-odd permutation.
 
     Example
     -------
-    >>> w, h = 4, 3
-    >>> seq = range(w*h)
-    >>> succ = partial(succ_of_rotate90cw, w=w, h=h)
-    >>> [succ(i) for i in seq]
-    [2, 5, 8, 11, 1, 4, 7, 10, 0, 3, 6, 9]
+    >>> seq = range(10)
+    >>> succ = partial(succ_of_even_odd, n=len(seq))
+    >>> print [succ(x) for x in seq]
+    [0, 5, 1, 6, 2, 7, 3, 8, 4, 9]
     """
-    # w:h to h:w (rotate90cw)
-    x = i%w
-    y = i/w
-    return x*h + (h-1)-y    # img[x][(h - 1) -  y]
+    if x%2 == 0:
+        return x/2
+    return n/2 + x/2
 
+#------------------------------------------------------------------------------
+# Apply Permutations to Picture Rotations
+#------------------------------------------------------------------------------
 
 def pred_of_rotate90cw(i, w, h):
     """Return predecessor of i in the cycle of a permutation of rotation 90
@@ -273,25 +280,25 @@ def pred_of_rotate90cw(i, w, h):
     y = i/h
     return (h-1 - x)*w + y  # img[(h - 1) - x][y]
 
-#------------------------------------------------------------------------------
 
-def succ_of_rotate90ccw(i, w, h):
+def succ_of_rotate90cw(i, w, h):
     """Return successor of i in the cycle of a permutation of rotation 90
-    degree counterclockwise.
+    degree clockwise.
 
     Example
     -------
     >>> w, h = 4, 3
     >>> seq = range(w*h)
-    >>> succ = partial(succ_of_rotate90ccw, w=w, h=h)
+    >>> succ = partial(succ_of_rotate90cw, w=w, h=h)
     >>> [succ(i) for i in seq]
-    [9, 6, 3, 0, 10, 7, 4, 1, 11, 8, 5, 2]
+    [2, 5, 8, 11, 1, 4, 7, 10, 0, 3, 6, 9]
     """
-    # w:h to h:w (rotate90ccw)
+    # w:h to h:w (rotate90cw)
     x = i%w
     y = i/w
-    return (w-1 - x)*h + y  # img[(w - 1) - x][y]
+    return x*h + (h-1)-y    # img[x][(h - 1) -  y]
 
+#------------------------------------------------------------------------------
 
 def pred_of_rotate90ccw(i, w, h):
     """Return predecessor of i in the cycle of a permutation of rotation 90
@@ -310,14 +317,25 @@ def pred_of_rotate90ccw(i, w, h):
     y = i/h
     return x*w + (w-1)-y    # img[x][(w - 1) -  y]
 
-#------------------------------------------------------------------------------
+    
+def succ_of_rotate90ccw(i, w, h):
+    """Return successor of i in the cycle of a permutation of rotation 90
+    degree counterclockwise.
 
-def succ_of_rotate180(i, w, h):
-    """Return successor of i in the cycle of a permutation of rotation 180
-    degree.
+    Example
+    -------
+    >>> w, h = 4, 3
+    >>> seq = range(w*h)
+    >>> succ = partial(succ_of_rotate90ccw, w=w, h=h)
+    >>> [succ(i) for i in seq]
+    [9, 6, 3, 0, 10, 7, 4, 1, 11, 8, 5, 2]
     """
-    return pred_of_rotate180(i, w, h)
+    # w:h to h:w (rotate90ccw)
+    x = i%w
+    y = i/w
+    return (w-1 - x)*h + y  # img[(w - 1) - x][y]
 
+#------------------------------------------------------------------------------
 
 def pred_of_rotate180(i, w, h):
     """Return predecessor of i in the cycle of a permutation of rotation 180
@@ -336,14 +354,21 @@ def pred_of_rotate180(i, w, h):
     y = i/w
     return (h-1-y)*w + (w-1)-x  # img[(h - 1) - y][(w - 1) - x]
 
+
+def succ_of_rotate180(i, w, h):
+    """Return successor of i in the cycle of a permutation of rotation 180
+    degree.
+    """
+    return pred_of_rotate180(i, w, h)
+
 #------------------------------------------------------------------------------
 # Demonstration
 #------------------------------------------------------------------------------
 
 def demo():
     w, h = 4, 3
-    succ = partial(succ_of_rotate90cw, w=w, h=h)
     pred = partial(pred_of_rotate90cw, w=w, h=h)
+    succ = partial(succ_of_rotate90cw, w=w, h=h)
 
     seq = range(w*h)
     one_line = [pred(i) for i in seq]   # a permutation in one-line notation
@@ -353,22 +378,22 @@ def demo():
     order = order_from_cycles(cycles)
     print 'order:', order
 
-    print '\nTest permute():'
+    print '\nTest permute_with_one_line():'
     seq = range(w*h)
     for i in range(order):
-        seq = permute(seq, one_line)
+        seq = permute_with_one_line(seq, one_line)
         print seq
 
-    print '\nTest permute_with_follow_cycles():'
+    print '\nTest permute_with_cycles():'
     seq = range(w*h)
     for i in range(order):
-        permute_with_follow_cycles(seq, cycles)
+        permute_with_cycles(seq, cycles)
         print seq
 
-    print '\nTest permute_with_modified_follow_cycles():'
+    print '\nTest permute_with_pred_and_succ():'
     seq = range(w*h)
     for i in range(order):
-        permute_with_modified_follow_cycles(seq, succ, pred)
+        permute_with_pred_and_succ(seq, pred, succ)
         print seq
 
 
