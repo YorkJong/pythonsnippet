443ab02a756906f54b0334870bb3ffcb4022e9f8
York <yukuan.jiang@gmail.com>
2012-06-06 00:43:06 +0800

    Changed for passing checks of the linter

--- mlang/char_lst.py
+++ mlang/char_lst.py
@@ -16,38 +16,38 @@ def divide(sequence, modulus=32):
     I = range(len(sequence))
     A = I[::modulus]
     B = A[1:] + [len(sequence)]
-    return [sequence[a:b] for a, b in zip(A,B)]
+    return [sequence[a:b] for a, b in zip(A, B)]
 
 #------------------------------------------------------------------------------
 
 Tbl = {
-    'latin1':{
-        'head':[
+    'latin1': {
+        'head': [
             '# ISO8859-1 (Latin-1)',
             '# ref. http://en.wikipedia.org/wiki/ISO/IEC_8859-1'
         ],
-        'code':'iso8859-1',
-        'range':((0x20,0x7F), (0xA0,0x0100))
+        'code': 'iso8859-1',
+        'range': ((0x20, 0x7F), (0xA0, 0x0100))
     },
-    'latin9':{
-        'head':[
+    'latin9': {
+        'head': [
             '# ISO8859-15 (Latin-9)',
             '# ref. http://en.wikipedia.org/wiki/ISO/IEC_8859-15'
          ],
-        'code':'iso8859-15',
-        'range':((0x20,0x7F), (0xA0,0x0100))
+        'code': 'iso8859-15',
+        'range': ((0x20, 0x7F), (0xA0, 0x0100))
     },
-    'cp1252':{
-        'head':[
+    'cp1252': {
+        'head': [
             '# Windows-1252 (cp1252)',
             '# ref. http://en.wikipedia.org/wiki/Windows-1252'
          ],
-        'code':'cp1252',
-        'range':(
-            (0x20,0x7F),
-            (0x80,0x81), (0x82,0x8D), (0x8E,0x8F),
-            (0x91,0x9D), (0x9E,0xA0),
-            (0xA0,0x0100)
+        'code': 'cp1252',
+        'range': (
+            (0x20, 0x7F),
+            (0x80, 0x81), (0x82, 0x8D), (0x8E, 0x8F),
+            (0x91, 0x9D), (0x9E, 0xA0),
+            (0xA0, 0x0100)
         )
     }
 }
@@ -60,7 +60,7 @@ def save_printable_chars(char_set='latin1'):
     lines = Tbl[char_set]['head']
     for a, b in Tbl[char_set]['range']:
         lines.extend(['', ':0x%02X' % a])
-        b = ''.join([chr(x) for x in range(a,b)])
+        b = ''.join([chr(x) for x in range(a, b)])
         u = b.decode(Tbl[char_set]['code'])
         lines.extend(divide(u))
 
